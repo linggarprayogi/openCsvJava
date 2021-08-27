ed9a018a7d1c524cf79cfa30c8876af104d77f21
linggarprayogi <linggar.prayogi@gmail.com>
2021-08-27 17:31:10 +0700

    new : project membaca ile scv dengan java

---
+++ src/main/java/readCsvFile/Main.java
@@ -0,0 +1,37 @@
+package readCsvFile;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String fileName = "D:\\EDU\\JAVA Native\\IO\\OpenCSV\\DataKaryawan.csv";
+		try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+//			List<String[]> r = reader.readAll();
+//			r.forEach(x -> System.out.println(Arrays.toString(x)));
+
+			String[] lineInArray;
+			int i = 0;
+			try {
+				while ((lineInArray = reader.readNext()) != null) {
+					i++;
+					if (i <= 1) {
+						continue;
+					}
+					System.out.println(lineInArray[0]);
+				}
+			} catch (CsvValidationException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
